4312314a4b6a8e4c08eb213b1bde72c4b5d9fa8f
louiseluli <123265274+louiseluli@users.noreply.github.com>
2024-08-03 01:17:56 -0300

    Create exercicio05.py
    Add desafio 5

---
+++ exercicio05/exercicio05.py
@@ -0,0 +1,7 @@
+# Crieduastuplas.Concatene-asparaformarumanovatupla.
+
+tupla1 = (input("Digite uma informação pra colocar na tupla 1: "))
+tupla2 = (input("Digite uma informação pra colocar na tupla 2: "))
+print(tupla1, tupla2)
+tupla3 = tupla1 + tupla2
+print(tupla3)
